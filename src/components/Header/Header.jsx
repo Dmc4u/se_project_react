b04d15eed8e8a7d4ba93bb65a56744e98abd5f75
Dmc4u <dmsesbiz2005@gmail.com>
2025-02-28 20:26:11 +0200

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,3 @@
-// import "./Header.css";
-// import logo from "../../assets/logo.svg";
-// import avatar from "../../assets/avatar.png";
-
-// function Header({ handleAddClick, weatherData }) {
-//   const currentDate = new Date();
-//   const options = { month: "long", day: "numeric" }; 
-//   const formattedDate = currentDate.toLocaleDateString("en-US", options);
-
-//   return (
-//     <header className="header">
-//       <img src={logo} alt="WTWR" className="header__logo" />
-//       <p className="header__date-and-location">
-//         {formattedDate}, {weatherData.city || "Loading..."}
-//       </p>
-//       <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">
-//         + Add Clothes
-//       </button>
-//       <div className="header__user-container">
-//         <p className="header__username">Terrence Tegegne</p>
-//         <img src={avatar} alt="User Avatar" className="header__avatar" />
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Header;
-
 import { useState } from "react";
 import "./Header.css";
 import logo from "../../assets/logo.svg";
@@ -45,25 +17,47 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <img src={logo} alt="WTWR" className="header__logo" />
+
       <p className="header__date-and-location">
         {formattedDate}, {weatherData.city || "Loading..."}
       </p>
+
       <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">
         + Add Clothes
       </button>
-      <div className="header__user-container">
+
+      <div className="header__user-container" onClick={toggleMobileMenu}>
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="User Avatar" className="header__avatar" />
       </div>
+
       <button onClick={toggleMobileMenu} className="header__menu-btn">
         {isMobileMenuOpened ? "Close" : "Menu"}
       </button>
-      <nav className={isMobileMenuOpened ? "nav-open" : "nav-closed"}>
-        {/* Add your navigation items here */}
-      </nav>
+
+      {isMobileMenuOpened && (
+        <nav className="nav-open">
+          <div className="header__menu-content">
+            <p className="header__menu-date">{formattedDate}, {weatherData.city || "Loading..."}</p>
+            <img src={avatar} alt="User Avatar" className="header__menu-avatar" />
+            <p className="header__menu-username">Terrence Tegegne</p>
+            <ul className="header__menu-list">
+              <li>
+                <button
+                  onClick={() => {
+                    handleAddClick();
+                    setIsMobileMenuOpened(false); // Close menu after clicking
+                  }}
+                >
+                  + Add Clothes
+                </button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
     </header>
   );
 }
 
 export default Header;
-
